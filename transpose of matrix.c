dd312dec5713ef7b9434d5de8189cc3ab148b945
Prashant Choudhary <143925781+PrashantChoudhary13579@users.noreply.github.com>
2023-12-11 22:20:17 +0530

    Add files via upload

---
+++ transpose of matrix.c
@@ -0,0 +1,32 @@
+// Write a program to transpose of  a  matrix //
+#include<stdio.h>
+int main()
+{
+	int a[3][3], b[3][3];
+	int i,j;
+	printf("Enter the value of matrix");
+	for(i=0;i<3;i++)
+	{
+		for(j=0;j<3;j++)
+		{
+			scanf("%d",&a[i][j]);
+		}
+	}
+	for(i=0;i<3;i++)
+	{
+		for(j=0;j<3;j++)
+		{
+			b[i][j]=a[j][i];
+		}
+	}
+	printf("Transpose of matrix is \n");
+	for(i=0;i<3;i++)
+	{
+		for(j=0;j<3;j++)
+		{
+			printf("%d\t",b[i][j]);
+		}
+		printf("\n");
+	}
+	return 0;
+}
