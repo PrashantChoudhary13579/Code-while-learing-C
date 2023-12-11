51eb428969255b5a7bbff0d240d24475ead7592b
Prashant Choudhary <143925781+PrashantChoudhary13579@users.noreply.github.com>
2023-12-11 22:15:17 +0530

    Add files via upload

---
+++ multiplication of two matrix.c
@@ -0,0 +1,39 @@
+//Multiplication of 2 matrix//
+#include<stdio.h>
+int main()
+{
+	int a[3][3],b[3][3],c[3][3];
+	int i,j,k,ra,ca,rb,cb;
+	printf("Enter the rows and columns of A matrix");
+	scanf("%d%d",&ra,&ca);
+	printf("Enter the rows and columns of B matrix");
+	scanf("%d%d",&rb,&cb);
+	for(i=0;i<3;i++)
+	{
+		for(j=0;j<3;j++)
+		{
+			scanf("%d",a[i][j]);
+		}
+	}
+	for(i=0;i<3;i++)
+	{
+		for(j=0;j<3;j++)
+		{
+			scanf("%d",b[i][j]);
+		}
+	}
+	//for multiplication
+	for(i=0;i<3;i++)
+	{
+		for(j=0;j<3;j++)
+		{
+			c[i][j]=0;
+			for(k=0;k<3;k++)
+			{
+				c[i][j]=c[i][j] + a[i][j]*b[k][j];
+			}
+		}
+	}
+	return 0;
+	
+}
