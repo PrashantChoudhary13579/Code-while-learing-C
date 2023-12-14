608f7e651ccf410464d9321a3301ed205a7a3851
Prashant Choudhary <143925781+PrashantChoudhary13579@users.noreply.github.com>
2023-12-14 21:55:41 +0530

    Add files via upload

---
+++ sum of two matrix.c
@@ -0,0 +1,42 @@
+// Write a program to print the sum of two matrix//
+#include<stdio.h>
+int main()
+{
+	int a[3][3],b[3][3],c[3][3];
+	int i,j;
+	printf("Enter two matrix for adding them");
+	for(i=0;i<3;i++)
+	{
+		for(j=0;j<3;j++)
+		{
+			scanf("%d",&a[i][j]);
+		}
+	}
+	for(i=0;i<3;i++)
+	{
+		for(j=0;j<3;j++)
+		{
+			scanf("%d",&b[i][j]);
+		}
+	}
+	
+	for(i=0;i<3;i++)
+	{
+		for(j=0;j<3;j++)
+		{
+			c[i][j]=a[i][j]+b[i][j];
+		}
+	}
+	printf("The sum of the two matrix are \n");
+	for(i=0;i<3;i++)
+	{
+		for(j=0;j<3;j++)
+		{
+			printf("%d\t",c[i][j]);
+		}
+		printf("\n");
+	}
+	return 0;
+	
+}
+
