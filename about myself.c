d31662b79b6e3888d90f8480fe8a6e2cfc60c133
PrashantChoudhary13579 <143925781+PrashantChoudhary13579@users.noreply.github.com>
2023-12-28 14:05:14 +0530

    Add enter in last line

--- about myself.c
+++ about myself.c
@@ -4,6 +4,6 @@ int main()
 {
 	printf("My name is Prashant Choudhary.\n");
 	printf("I am studing in UIET,Chandigarh.\n");
-	printf("I am pursuing the branch of CSE.");
+	printf("I am pursuing the branch of CSE.\n");
 	return 0;
 }
